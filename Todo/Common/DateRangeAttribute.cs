f64afe7f8664e7e03af82531860648f3e90037ac
Makesh-Nils <bhbtheemployee@gmail.com>
2021-06-09 11:09:54 +0530

    New Update

---
+++ Todo/Common/DateRangeAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Todo.Common
+{
+    public class DateRangeAttribute : RangeAttribute
+    {       
+
+        public DateRangeAttribute(string maxvalue) : base(typeof(DateTime),DateTime.Now.ToShortDateString(),maxvalue)
+        {
+
+        }
+        /*public override bool IsValid(object value)
+        {
+            DateTime propValue = Convert.ToDateTime(value);
+
+            return propValue > DateTime.Now;
+           
+        }*/
+    }
+}
---
+++ Todo/Controllers/ExpenseController.cs
@@ -0,0 +1,112 @@
+﻿using Todo.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Todo.Models;
+
+namespace Todo.Controllers
+{
+    public class ExpenseController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public ExpenseController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Expense> objList = _db.Expense;
+            return View(objList);
+        }
+
+        //Get Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        //Post Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Expense obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Expense.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+        //Get delete
+        public IActionResult Delete(int id)
+        {        
+
+            var obj = _db.Expense.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+
+        }
+
+        //Post Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int id)
+        {
+            var obj = _db.Expense.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            
+            _db.Expense.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+           
+        }
+
+        //Get update
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Expense.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+
+        }
+
+
+        //Post update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Expense obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Expense.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+    }
+}
